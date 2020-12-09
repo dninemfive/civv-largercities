6f32c734ddb7e7765ce516a91ca65c7b0763be18
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-12-09 10:27:18 -0800

    code cleanup and commenting

--- Patches_VMC.sql
+++ Patches_VMC.sql
@@ -1,7 +1,10 @@
+/*
+Changes the distance at which cities can work and buy tiles if Various Mod Components (or another DLL) allows doing so.
+*/
 UPDATE Defines
 	SET Value=5 
 	WHERE Name='MAXIMUM_WORK_PLOT_DISTANCE'
-	AND EXISTS (SELECT Value FROM Defines WHERE Name='MAXIMUM_WORK_PLOT_DISTANCE');
+	AND EXISTS (SELECT Value FROM Defines WHERE Name='MAXIMUM_WORK_PLOT_DISTANCE'); -- MAXIMUM_WORK_PLOT_DISTANCE exists only if VMC (or another DLL) adds it
 	SET Value=5 
 	WHERE Name='MAXIMUM_BUY_PLOT_DISTANCE'
-	AND EXISTS (SELECT Value FROM Defines WHERE Name='MAXIMUM_BUY_PLOT_DISTANCE');
+	AND EXISTS (SELECT Value FROM Defines WHERE Name='MAXIMUM_BUY_PLOT_DISTANCE');  -- Ditto.
